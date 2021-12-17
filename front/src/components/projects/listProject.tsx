e6c1e1df4e11c51ce55c9113d370b0bc7b5c1ed4
Jun Naruse <jun.naruse@gmail.com>
2021-12-16 23:47:11 -0500

    [UPD] CSS for project, experience, education and tools

--- front/src/components/projects/listProject.tsx
+++ front/src/components/projects/listProject.tsx
@@ -23,11 +23,11 @@ const ListProject = ({project}:{project:Project}) => {
                     </div>
                     <div className='row card-links'>
                         <a onClick={eventTrack.bind(this, project.name, 'click', 'front')} href={project.front_url} 
-                            className="btn col-4" target="_blank"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon> Front</a>
+                            className="btn col-4" target="_blank"><FontAwesomeIcon icon={faGithub} className='d-none d-sm-block'></FontAwesomeIcon>  Front</a>
                         <a onClick={eventTrack.bind(this, project.name, 'click', 'back')} href={project.back_url} 
-                            className="btn col-4" target="_blank"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon> Back</a>
+                            className="btn col-4" target="_blank"><FontAwesomeIcon icon={faGithub} className='d-none d-sm-block'></FontAwesomeIcon>  Back</a>
                         <a onClick={eventTrack.bind(this, project.name, 'click', 'demo')} href={project.demo_url} 
-                            className="btn col-4" target="_blank"><FontAwesomeIcon icon={faChrome}></FontAwesomeIcon> Demo</a>
+                            className="btn col-4" target="_blank"><FontAwesomeIcon icon={faChrome} className='d-none d-sm-block'></FontAwesomeIcon>  Demo</a>
                     </div>
                 </div>
             </div>
