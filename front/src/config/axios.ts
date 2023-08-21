6957ef457a43eee202776a90bce7e1cbef9575bb
Jun Naruse <jun.naruse@gmail.com>
2023-08-20 23:06:52 -0500

    [FIX] Change from http to https

--- front/src/config/axios.ts
+++ front/src/config/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const clientAxios = axios.create({
     baseURL: process.env.NODE_ENV === 'production'
-        ? 'http://app.junnaruse.com/gpt/' // return prod api if running in prod
+        ? 'https://app.junnaruse.com/gpt/' // return prod api if running in prod
         : 'http://localhost:8000/' // return local api if running locally
 });
 
