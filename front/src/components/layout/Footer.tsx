41cb8e8cc6f640288c866267c15d9e4b7b814b2b
Jun Naruse <jun.naruse@gmail.com>
2023-08-26 22:23:15 -0500

    [UPD] Remove contact form

--- front/src/components/layout/Footer.tsx
+++ front/src/components/layout/Footer.tsx
@@ -31,12 +31,12 @@ const Footer = () => {
                     <a className='col-sm-3 col-6 contact-link' href={medias[0].contact} target='_blank'><p><FontAwesomeIcon icon={faGithub}/> {medias[0].description}</p></a>
                     <a className='col-sm-3 col-6 contact-link' href={medias[1].contact} target='_blank'><p><FontAwesomeIcon icon={faGitlab}/> {medias[1].description}</p></a>
                     <a className='col-sm-3 col-6 contact-link' href={medias[2].contact} target='_blank'><p><FontAwesomeIcon icon={faLinkedin}/> {medias[2].description}</p></a>
-                    <a className='col-sm-3 col-6 contact-link' href={'#ContactForm'} onClick={updateState}><p><FontAwesomeIcon icon={faEnvelope}/> {medias[3].description}</p></a>
+                    <a className='col-sm-3 col-6 contact-link' href={`mailto:${medias[3].contact}`} onClick={updateState}><p><FontAwesomeIcon icon={faEnvelope}/> {medias[3].description}</p></a>
                 </div>
-                <hr className={`mb-4 ${contactFormState}`}/>
+                {/* <hr className={`mb-4 ${contactFormState}`}/>
                 <div className={`row ${contactFormState}`}>
                     <Email myEmail={`mailto:${medias[3].contact}`}/>
-                </div>
+                </div> */}
                 
             </div>
         </footer>
